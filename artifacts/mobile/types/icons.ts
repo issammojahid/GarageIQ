7b7b6ed537aada5438674c661106bfb4aa40f0e8
issammojahid1230-ai <issammojahid1230@gmail.com>
2026-03-28 18:20:29 +0100

    update: fixed bugs and added features

--- artifacts/mobile/types/icons.ts
+++
@@ -1,4 +0,0 @@
-import type { MaterialCommunityIcons } from "@expo/vector-icons";
-import type React from "react";
-
-export type MaterialCommunityIconsName = React.ComponentProps<typeof MaterialCommunityIcons>["name"];
--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./vehicles";
-export * from "./diagnoses";
-export * from "./fuel_logs";
-export * from "./maintenance";
-export * from "./documents";
-export * from "./conversations";
-export * from "./messages";
